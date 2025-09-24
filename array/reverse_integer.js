1eb3cc5932b0e6490f9b818b5a1d86b451fe2853
ekrmabd <ekramhossainekram28@gmail.com>
2025-09-24 12:03:07 +0600

    reverse integer leetcode 7

--- array/reverse_integer.js
+++ array/reverse_integer.js
@@ -1,16 +1,32 @@
 //7. Reverse Integer
+var reverse = function(x) {
+    let numStatus = x < 0 ? -1 : 1;
+    x = Math.abs(x);
 
-var moveZeroes = function(nums) {
-	let lastNonZero = 0;
-	for(let i = 0; i < nums.length; i++){
-		if(nums[i] != 0){
-			let temp = nums[i];
-			nums[i] = nums[lastNonZero];
-			nums[lastNonZero] = temp;
-			lastNonZero++;
-		}
-	}
-	return nums;
+    let arr = String(x).split("");
+    let left = 0, right = arr.length - 1;
+
+    while (left < right) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+
+    let result = parseInt(arr.join("")) * numStatus;
+
+
+    if (result < -(2**31) || result > (2**31 - 1)) {
+        return 0;
+    }
+
+    return result;
 };
 
-console.log(moveZeroes([0,1,0,3,12]));
+
+console.log(reverse(123));
+console.log(reverse(120));
+console.log(reverse(-123));
+console.log(reverse(1534236469)); 
+
